463aa164d5a2eb08ec7b609fb94e82ac244a459f
seoyoung <choisy9619@gmail.com>
2024-06-23 00:32:06 +0900

    회원 서비스 개발

--- src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -1,12 +1,11 @@
 package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import static org.assertj.core.api.Assertions.*;
 
